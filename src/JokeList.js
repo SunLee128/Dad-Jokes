9f7d7aa9dabd783fa225944a2bcbf3ca843deeda
sunlee <sunlee128@gmail.com>
2020-01-19 07:33:12 +1100

    Jokelist completed

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,32 +1,54 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Joke from './Joke'
+import React, { Component } from "react";
+import Joke from "./Joke";
+import axios from "axios";
 import uuid from "uuid/v4";
-import './JokeList.css'
+import "./JokeList.css";
 
-class JokeList extends Component  {
+class JokeList extends Component {
   static defaultProps = {
     numJokesToGet: 10
-  }
+  };
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      jokes: []
-    }
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+      loading: false
+    };
+    //using set for performance
+    this.seenJokes = new Set(this.state.jokes.map(j => j.text));
+    console.log(this.seenJokes);
+    this.handleClick = this.handleClick.bind(this);
   }
-  async componentDidMount() {
-    //get 10 jokes
-    let jokes=[]
-    while(jokes.length < this.props.numJokesToGet){
-      let res = await axios.get("https://icanhazdadjoke.com/",{
-      headers: {Accept: "application/json "}
-      })
-      jokes.push({text:res.data.joke, votes:0 })
-    }
-    this.setState({jokes: jokes})
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.getJokes();
   }
-  handleClick() {
-    this.setState({ loading: true }, this.getJokes);
+  async getJokes() {
+    try {
+      let jokes = [];
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com/", {
+          headers: { Accept: "application/json" }
+        });
+        let newJoke = res.data.joke;
+        if (!this.seenJokes.has(newJoke)) {
+          jokes.push({ id: uuid(), text: newJoke, votes: 0 });
+        } else {
+          console.log("FOUND A DUPLICATE!");
+          console.log(newJoke);
+        }
+      }
+      this.setState(
+        st => ({
+          loading: false,
+          jokes: [...st.jokes, ...jokes]
+        }),
+        () =>
+          window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+      );
+    } catch (e) {
+      alert(e);
+      this.setState({ loading: false });
+    }
   }
   handleVote(id, delta) {
     this.setState(
@@ -39,23 +61,44 @@ class JokeList extends Component  {
         window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
     );
   }
+  handleClick() {
+    this.setState({ loading: true }, this.getJokes);
+  }
   render() {
-    return (
-      <div className="JokeList">
-        <div className="JokeList-sidebar">
-          <h1 className="JokeList-title">
-            <span>Dad</span>Jokes
-          </h1>
-          <button className='JokeList-getmore' onClick={this.handleClick} >New Jokes</button>
+    if (this.state.loading) {
+      return (
+        <div className='JokeList-spinner'>
+          <i className='far fa-8x fa-laugh fa-spin' />
+          <h1 className='JokeList-title'>Loading...</h1>
         </div>
-        <div className="JokeList-jokes">
-          {this.state.jokes.map(j => (
-            <Joke votes={j.votes} text={j.text}/>
+      );
+    }
+    let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
+    return (
+      <div className='JokeList'>
+        <div className='JokeList-sidebar'>
+          <h1 className='JokeList-title'>
+            <span>Dad</span> Jokes
+          </h1>
+          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+          <button className='JokeList-getmore' onClick={this.handleClick}>
+            Fetch Jokes
+          </button>
+        </div>
+
+        <div className='JokeList-jokes'>
+          {jokes.map(j => (
+            <Joke
+              key={j.id}
+              votes={j.votes}
+              text={j.text}
+              upvote={() => this.handleVote(j.id, 1)}
+              downvote={() => this.handleVote(j.id, -1)}
+            />
           ))}
         </div>
       </div>
-    )
+    );
   }
 }
-
-export default JokeList
+export default JokeList;
